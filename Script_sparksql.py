d0aaee683a4572525ef71e09e9001207b665d69c
Ronan Niel <22302110@me.uvsq.fr>
2025-02-03 11:03:24 +0100

    envoie

--- Script_sparksql.py
+++ Script_sparksql.py
@@ -10,70 +10,75 @@ def demande_acteur ():
     while i<question:
         liste_acteur.append(input("donnez un nom d'acteur : "))
         i+=1
-demande_acteur()
+    return liste_acteur
+
 directory = "./data/"
 if not os.path.exists(directory):
     os.makedirs(directory)
 
-sparql = SPARQLWrapper("http://dbpedia.org/sparql")
+sparql = SPARQLWrapper("https://query.wikidata.org/sparql")
 
 # SELECT query for JSON, XML, CSV, TSV formats
 select_query = """
-PREFIX wikibase: <http://wikiba.se/ontology#>
 PREFIX wdt: <http://www.wikidata.org/prop/direct/>
 PREFIX wd: <http://www.wikidata.org/entity/>
-PREFIX rdfs: <http://www.w3.org/2000/01/rdf-schema#>
-PREFIX p: <http://www.wikidata.org/prop/statement/>
+PREFIX p: <http://www.wikidata.org/prop/>
+PREFIX ps: <http://www.wikidata.org/prop/statement/>
 PREFIX pq: <http://www.wikidata.org/prop/qualifier/>
-PREFIX ps: <http://www.wikidata.org/prop/statement/value/>
+PREFIX wikibase: <http://wikiba.se/ontology#>
 PREFIX bd: <http://www.bigdata.com/rdf#>
-SELECT DISTINCT ?movie ?title (YEAR(?releaseDate) AS ?releaseYear) ?genreFilmLabel 
-?casting_acteur ?casting_Birth ?casting_role
-?directorLabel ?directorBirthDate
+
+SELECT ?movie ?title (YEAR(MIN(?releaseDate)) AS ?releaseYear)
+       ?casting_acteur ?casting_Birth 
+       (SAMPLE(?casting_roleLabel) AS ?casting_name) 
+       (SAMPLE(?directorLabel) AS ?directorName) 
+       (SAMPLE(?directorBirthDate) AS ?directorBirth) 
+       (GROUP_CONCAT(DISTINCT ?genreFilmLabel; separator=", ") AS ?genres)
 WHERE {
-  ?varActeur rdfs:label "Louis de Funès"@fr.
-  
-  # Films où Louis de Funès est acteur
+  ?varActeur rdfs:label "Andrew Garfield"@fr.
+
+  # Films où la personne est acteur
   ?movie wdt:P161 ?varActeur;
          wdt:P31 wd:Q11424 ;          # Instance de : Film
          wdt:P1476 ?title ;           # Titre du film
          wdt:P577 ?releaseDate ;      # Date de sortie
          wdt:P136 ?genreFilm.         # Genre du film
-  
+
   # Récupération des labels pour les genres
-  SERVICE wikibase:label {
-    bd:serviceParam wikibase:language "fr". # Priorité au français pour les labels
+  SERVICE wikibase:label { 
+    bd:serviceParam wikibase:language "fr". 
     ?genreFilm rdfs:label ?genreFilmLabel.
   }
-  
-  # Nom du personnage joué par Louis de Funès
+
+  # Nom du personnage joué par l'acteur
   OPTIONAL {
-    ?movie p:P161 ?castingStatement.           # Déclaration de casting
-    ?castingStatement ps:P161 ?varActeur;        # Louis de Funès est l'acteur
-                       pq:P453 ?roleEntity.    # Propriété "joue le rôle de"
-    ?roleEntity rdfs:label ?casting_role. FILTER(LANG(?casting_role) = "fr")
+    ?movie p:P161 ?castingStatement.
+    ?castingStatement ps:P161 ?varActeur; 
+                       pq:P453 ?roleEntity.  # P453 pour le rôle joué
+    ?roleEntity rdfs:label ?casting_roleLabel. FILTER(LANG(?casting_roleLabel) = "fr")
   }
 
-  # Données sur Louis de Funès (acteur)
-  BIND(?varActeur AS ?casting_acteur)    # Nom explicite
-  OPTIONAL { wd:Q2737 wdt:P569 ?casting_Birth. } # Date de naissance de Louis de Funès
+  # Données sur l'acteur
+  BIND("Andrew Garfield" AS ?casting_acteur)
+  OPTIONAL { ?varActeur wdt:P569 ?casting_Birth. } # Date de naissance de l'acteur
 
   # Données optionnelles sur le réalisateur
   OPTIONAL {
     ?movie wdt:P57 ?director.
     ?director rdfs:label ?directorLabel. FILTER(LANG(?directorLabel) = "fr")
-    
-    # Date de naissance du réalisateur
     OPTIONAL { ?director wdt:P569 ?directorBirthDate. }
   }
 }
+GROUP BY ?movie ?title ?casting_acteur ?casting_Birth ?casting_roleLabel
 ORDER BY ?releaseYear
+LIMIT 3
 
 
 """
-sparql.setQuery(select_query)
-print('\n\n*** CSV Example')
-sparql.setReturnFormat(CSV)
+
+
+print('\n\n*** XML Example')
+sparql.setReturnFormat(XML)
 results = sparql.query().convert()
-with open(os.path.join(directory, "results.csv"), "w", encoding="utf-8") as f:
-    f.write(results.decode("utf-8"))
+with open(os.path.join(directory, "results.xml"), "w", encoding="utf-8") as f:
+    f.write(results.toxml())
